35a9088a36a1d64d1de74c354c950abd8b018a0a
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2023-11-15 20:22:17 +0100

    fix: concurrent redis set

--- src/data/cache.ts
+++ src/data/cache.ts
@@ -1,16 +1,28 @@
 import { Redis } from "ioredis";
+
 import { loadAndParseCSV } from "./load.js";
+import { Player } from "./types.js";
+
+/* this could be called directly from
+   createNewPlayer to speed up loading into redis
+*/
+async function setNewPlayer(redis: Redis, p: Player) {
+  return redis.set(p.NAME, JSON.stringify(p), (err) => {
+    if (err) {
+      console.error(`setting player ${p.NAME} to redis failed: ${err}`);
+    }
+  });
+}
 
 export async function loadDataIntoRedis(redisUrl: string) {
   const playersArray = await loadAndParseCSV();
   const redis = new Redis(redisUrl);
 
+  const okProm: Promise<"OK">[] = [];
   for (const p of playersArray) {
-    await redis.set(p.NAME, JSON.stringify(p), (err) => {
-      if (err) {
-        console.error(`setting player ${p.NAME} to redis failed: ${err}`);
-        return { message: err };
-      }
-    });
+    okProm.push(setNewPlayer(redis, p));
   }
+
+  await Promise.all(okProm);
+  await redis.quit();
 }
